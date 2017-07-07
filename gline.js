8ed62c55a5ffcc4b844cb951ba3896fed2b50915
sks1996 <getsuraj96@gmail.com>
2017-07-07 21:51:22 +0530

    first commit

---
+++ gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Year','umich.edu','virginia.edu','cam.ac.uk','unicon.net','mtu.edu','berkeley.edu','unl.edu','etudes.org','columbia.edu','ucmerced.edu'],
+['2005',8,5,4,4,3,3,3,2,2,2]
+];
